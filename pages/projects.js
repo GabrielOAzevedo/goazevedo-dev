58d30ee5886c32eaefe80937262b1f47948f16ef
Gabriel Azevedo <godeazevedo@gmail.com>
2021-03-28 13:59:31 -0300

    * Updated my projects (finally)

--- pages/projects.js
+++ pages/projects.js
@@ -5,16 +5,30 @@ export default function ProjectsPage(props) {
         <Layout>
             <div className="h-3/4 flex flex-col justify-center">
                 <div>
-                    <p>Whelp...</p>
-                    <p>I will bring my projects here soon...</p>
+                    <a href="https://inspector.goazevedo.dev/">
+                        <span className="text-purple-600 underline">React Component Inspector</span>
+                    </a>
+                    &nbsp;-&nbsp;
+                    <a target="_blank" href="https://github.com/GabrielOAzevedo/component-inspector">
+                        <span className="text-purple-600 underline">&lt;GitHub/&gt;</span>
+                    </a>
                     <p>
-                        Please follow me on&nbsp;
-                        <a target="_blank" href="https://github.com/GabrielOAzevedo">
-                            <span className="text-purple-600 underline">GitHub</span>
-                        </a>
-                        &nbsp;to get the latest updates
+                        This is the official repository of the best component inspector that I know of, 
+                        and I know a total of one. Please feel free to clone, create new functionality, 
+                        use it as your own or do whatever you want to do with this. This is a work in 
+                        progress project (WIP) and I will try my best to save it from github limbo, a 
+                        place where all personal projects eventually end.
                     </p>
                 </div>
+            </div>
+            <div>
+                <p>
+                    Please follow me on&nbsp;
+                    <a target="_blank" href="https://github.com/GabrielOAzevedo">
+                        <span className="text-purple-600 underline">GitHub</span>
+                    </a>
+                    &nbsp;to get the latest updates
+                </p>
             </div>
         </Layout>
     )
