e6c030703a69ad6cb0d5b05ce375106bec388a87
Gabriel Azevedo <godeazevedo@gmail.com>
2022-01-04 09:54:23 -0300

    updated main phrase

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import Image from 'next/image';
-import Layout from '../components/layout';
+import Image from "next/image";
+import Layout from "../components/layout";
 
 export default function Home() {
   return (
-    <Layout home> 
+    <Layout home>
       <section className="flex flex-col h-3/4 justify-center">
         <div className="ml-2">
           <p>Hi! I am</p>
-          <h1 className="text-purple-600 underline text-xl">Gabriel Azevedo</h1>
-          <p>and I am a full-stack developer.</p>
+          <h1 className="text-purple-600 underline text-xl">
+            Gabriel Azevedo
+          </h1>
+          <p>and I am someone that likes to write code.</p>
         </div>
         <div className="flex flex-row mt-4">
-          <a target="_blank" href="https://github.com/GabrielOAzevedo">
+          <a
+            target="_blank"
+            href="https://github.com/GabrielOAzevedo"
+          >
             <Image
               src="/images/github.svg"
               alt="my github"
@@ -19,7 +24,10 @@ export default function Home() {
               height={48}
             />
           </a>
-          <a target="_blank" href="https://www.instagram.com/gaaoliveir/">
+          <a
+            target="_blank"
+            href="https://www.instagram.com/gaaoliveir/"
+          >
             <Image
               src="/images/instagram.svg"
               alt="my instagram"
@@ -27,7 +35,10 @@ export default function Home() {
               height={48}
             />
           </a>
-          <a target="_blank" href="https://www.linkedin.com/in/goazevedo/">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/goazevedo/"
+          >
             <Image
               src="/images/linkedin.svg"
               alt="my github"
@@ -38,5 +49,5 @@ export default function Home() {
         </div>
       </section>
     </Layout>
-  )
+  );
 }
